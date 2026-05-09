ddae581e5964059c09620c083714d31f541b119f
miriam2612 <miriamcamacho2002@gmail.com>
2026-05-08 18:58:19 -0600

    nada

---
+++ hola.dart
@@ -0,0 +1,35 @@
+name: dmstride_prueba
+description: "A new Flutter project."
+publish_to: 'none'
+
+version: 1.0.0+1
+
+environment:
+  sdk: ^3.11.4
+
+dependencies:
+  flutter:
+    sdk: flutter
+  flutter_localizations:
+    sdk: flutter
+  cupertino_icons: ^1.0.8
+  firebase_core: ^3.6.0
+  firebase_auth: ^5.3.1
+  cloud_firestore: ^5.4.4
+  image_picker: ^1.1.2
+  camera: ^0.10.5+5
+  google_mlkit_object_detection: 0.14.0
+  google_mlkit_commons: ^0.9.0
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+  flutter_lints: ^6.0.0
+
+flutter:
+  uses-material-design: true
+
+  assets:
+    - assets/images/logo.jpeg
+    - assets/images/pie_izq.png
+    - assets/images/pie_der.png
